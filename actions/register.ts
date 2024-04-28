0c2d60d89fe2871f7b16e66368b7edf7b51f5fb8
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2024-04-28 16:00:09 -0700

    basic register functionality, e.g. password encryption,etc

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 import { RegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
@@ -11,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
             error: 'Invalid fields!',
         };
     }
-    return { success: 'Email sent!' };
+
+    const { email, password, name } = validateFields.data;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'Email already exists!', }
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            name,
+            password: hashPassword,
+        }
+    });
+
+    // TODO: Send verification token email
+    return { success: 'User Create!' };
 }
