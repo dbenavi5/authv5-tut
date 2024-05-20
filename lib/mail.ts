e5906824ea9bcd80e7861e9ec36b90f68c545ebe
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2024-05-20 14:55:56 -0700

    -- fix confirm link in mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -34,7 +34,7 @@ export const sendVerificationEmail = async (
     email: string,
     token: string,
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: 'mail@auth5guide.us',
